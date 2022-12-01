26be9173847cebb785227d988291144e79a5aa12
Sanjaya kumar Verma <118012270+sanjayguruji@users.noreply.github.com>
2022-12-02 04:59:40 +0530

    Create costume-condition-check.tf

---
+++ costume-condition-check.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "terra-server" {
+  instance_type = "t2.micro"
+  ami           = "ami-0g91b56"
+
+  lifecycle {
+    # The AMI ID must refer to an AMI that contains an operating system
+    # for the `x86_64` architecture.
+    precondition {
+      condition     = data.aws_ami.example.architecture == "x86_64"
+      error_message = "The selected AMI must be for the x86_64 architecture."
+    }
+  }
+}
