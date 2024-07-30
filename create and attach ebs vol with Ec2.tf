e4f00ad908b3833c65cc2df79492f677d5f25078
Sanjaya kumar Verma <118012270+sanjayguruji@users.noreply.github.com>
2024-07-30 13:49:49 +0530

    Create create and attach ebs vol with Ec2.tf

---
+++ create and attach ebs vol with Ec2.tf
@@ -0,0 +1,26 @@
+#create EC2 volume code
+resource "aws_instance" "web" {
+  ami               = "ami-068e0f1a600cd311c"
+  availability_zone = "ap-south-1a"
+  instance_type     = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+#create ebs vol code
+resource "aws_ebs_volume" "data-vol" {
+  availability_zone = "ap-south-1a"
+  size              = 16
+
+  tags = {
+    Name = "Ebs-Storage"
+  }
+}
+#create ebs attach code
+resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.data-vol.id
+  instance_id = aws_instance.web.id
+ }
